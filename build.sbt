a8feb9f1c706cce83a7e70d6df67d5dc5a57be7c
danimateos <dmateos.sanmartin@gmail.com>
2017-01-03 17:50:54 +0100

    First compile

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "mxnet-playground"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "ml.dmlc.mxnet" % "mxnet-full_2.10-linux-x86_64-cpu" % "0.1.1"
+
+
---
+++ src/main/scala/playground/TryOut.scala
@@ -0,0 +1,9 @@
+package playground
+
+import ml.dmlc.mxnet._
+import ml.dmlc.mxnet.optimizer.SGD
+
+object TryOut extends App {
+  // model definition
+  val data = Symbol.Variable("data")
+}
